02deb1e621cc7561204b05e78fbab5d791d57cab
Ubuntu <wojtek@cloud-assignment-vm.v5ozgergpcnejeqa2qvb34uh2c.zx.internal.cloudapp.net>
2022-04-25 13:31:21 +0000

    Push src directory

--- test.js
+++ test.js
@@ -1,21 +1,54 @@
-//first NodeJS app
-
 const express = require('express')
 const app = express()
 const port = 3000
 
+var os = require('os')
+var hostname = os.hostname()
+
 app.get('/', (req, res) => {
-    res.send('Hello Virtual Machine!')
+    res.send('Hello Virtual Machine! \n The hostname of the current node is: '+hostname)
 })
 
-app.get('/servicea', (req, res) => {
-    res.send('Service A')
-})
+const amqp = require('amqplib/callback_api');
+const exchange = 'logs';
+let channel = null;
 
-app.get('/serviceb', (req, res) => {
-    res.send('Service B')
-})
+//connect to rabbitmq
+ amqp.connect('amqp://test:test@20.108.23.201', function(error0, connection) {
+   if (error0) { throw error0;}
+   connection.createChannel(function(error1, ch) {
+       //setup the exchange
+       if (error1) { throw error1; }
+       channel = ch;
+       ch.assertExchange(exchange, 'fanout', { durable: false });
 
-app.listen(port, ()=> {
-    console.log('Express Application listening at port 3000')
-})
+       //listener
+       channel.assertQueue('', {exclusive: true}, function(error2, q) {
+         if (error2) { throw error2; }
+         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+         channel.bindQueue(q.queue, exchange, '');
+         channel.consume(q.queue, function(msg){
+           if(msg.content) {
+             console.log(" [x] %s", msg.content.toString());
+           }
+         }, {noAck: true});
+       });
+     });
+   });
+
+   // close the connection on exit
+   process.on('exit', (code) => {
+     if(channel!=null){
+       channel.close();
+       console.log("closing rabbitmq channel");
+     }
+   });
+
+// publisher - message every 3 seconds
+ setInterval(function() {
+   let message = "Hello world!!!";
+   if(channel){
+     channel.publish(exchange, '', Buffer.from(message));
+     console.log("console log test");
+   }
+ }, 3000);
